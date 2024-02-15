167377274a9331d0e04727f1b4540659a32df3d9
PedroCatelann <pedrohfktlan@gmail.com>
2024-02-15 16:57:41 -0300

    commit microservice email

---
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,24 @@
+package com.ms.user.services;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
+import com.ms.user.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired //Funciona como se fosse um construtor
+    UserRepository userRepository;
+    @Autowired
+    UserProducer userProducer;
+
+    @Transactional
+    public UserModel save(UserModel userModel) {
+
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
+    }
+}
